856b876e077fe22d5597b574fc8ce249a426ec4a
Aaron Gonzalez <135724364+AaronEGH16@users.noreply.github.com>
2026-02-08 12:59:35 -0300

    Update api.py

--- netlify/functions/api.py
+++ netlify/functions/api.py
@@ -1,14 +1,5 @@
-from fastapi import FastAPI
-from mangum import Mangum
-
-app = FastAPI()
-
-@app.get("/")
-def root():
-    return {"message": "Hola desde FastAPI en Netlify"}
-
-@app.get("/users/{user_id}")
-def get_user(user_id: int):
-    return {"user_id": user_id}
-
-handler = Mangum(app)
+def handler(event, context):
+    return {
+        "statusCode": 200,
+        "body": "hola"
+    }
